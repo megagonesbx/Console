ff6f3ebf1348ec0252f711892b154afa54041b2b
Javier Martínez <jmartinez@develsystems.com>
2023-04-23 16:27:01 -0600

    [UPDATE] Rules validators added to notification endpoints

--- Condominium.Console.Backend/src/routes/notification.ts
+++ Condominium.Console.Backend/src/routes/notification.ts
@@ -1,17 +1,41 @@
 import { Router } from "express";
 import { deleteNotification, getNotifications, sendNotification, updateNotification } from "../controllers";
+import { createNotificationValidationRules, getNotificationsValidationRules, notificationValidationRules } from "../validators";
+import { validateAdminRole, validateFields, validateJWT } from "../middlewares";
 
 const router = Router();
 
 router.post(
     '/create',
+    createNotificationValidationRules(),
+    validateJWT,
+    validateAdminRole,
+    validateFields,
     sendNotification
 );
 
-router.get('/', getNotifications);
+// TODO: CREATE MIDDLEWARE TO VALIDATE RESIDENT ROLE
+router.get(
+    '/:email',
+    getNotificationsValidationRules(),
+    validateJWT,
+    validateFields,
+    getNotifications
+);
 
-router.put('/viewed/:notificationId', updateNotification);
+router.put(
+    '/viewed/:notificationId',
+    notificationValidationRules(),
+    validateJWT,
+    validateFields,
+    updateNotification);
 
-router.delete('/delete/:notificationId', deleteNotification);
+router.delete(
+    '/delete/:notificationId', 
+    notificationValidationRules(),
+    validateJWT,
+    validateFields,
+    deleteNotification
+);
 
 export default router;
--- Condominium.Console.Backend/src/validators/index.ts
+++ Condominium.Console.Backend/src/validators/index.ts
@@ -1,6 +1,7 @@
 export * from './auth';
 export * from './donation';
 export * from './global';
+export * from './notification';
 export * from './resident';
 export * from './spreadsheet';
 export * from './user';
