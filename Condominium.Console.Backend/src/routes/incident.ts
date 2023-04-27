ff9136626523e1c90b227ea6c5621a1b688d3a7e
Javier Martínez <jmartinez@develsystems.com>
2023-04-26 19:06:50 -0600

    [UPDATE] Validations to incident routes

--- Condominium.Console.Backend/src/routes/incident.ts
+++ Condominium.Console.Backend/src/routes/incident.ts
@@ -2,30 +2,49 @@ import { Router } from "express";
 
 import { createIncident, getIncident, updateIncident, deleteIncident, getIncidents } from '../controllers';
 
+import { createIncidentValidationRules, getIncidentValidationRules, getIncidentsValidationRules, updateIncidentValidationRules } from "../validators/incident/incident-rules";
+import { validateFields, validateJWT } from "../middlewares";
+
 const router = Router();
 
+// TODO: CREATE MIDDLEWARE TO VALIDATE IF ROLE IS ADMIN OR OPERATOR
 router.get(
     '/:incidentId',
+    getIncidentValidationRules(),
+    validateJWT,
+    validateFields,
     getIncident
 );
 
 router.post(
     '/create',
+    createIncidentValidationRules(),
+    validateJWT,
+    validateFields,
     createIncident
 );
 
 router.put(
     '/update',
+    updateIncidentValidationRules(),
+    validateJWT,
+    validateFields,
     updateIncident
 );
 
 router.delete(
     '/:incidentId',
+    getIncidentValidationRules(),
+    validateJWT,
+    validateFields,
     deleteIncident
 );
 
 router.post(
     '/incidents',
+    getIncidentsValidationRules(),
+    validateJWT,
+    validateFields,
     getIncidents
 );
 
