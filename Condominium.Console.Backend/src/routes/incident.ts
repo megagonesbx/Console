4ef663bdd078374188f469f697d904f41d5f4083
Javier Martínez <jmartinez@develsystems.com>
2023-04-26 21:05:02 -0600

    [UPDATE] Swagger added to incident endpoints

--- Condominium.Console.Backend/src/routes/incident.ts
+++ Condominium.Console.Backend/src/routes/incident.ts
@@ -7,7 +7,44 @@ import { validateRole, validateFields, validateJWT } from "../middlewares";
 
 const router = Router();
 
-// TODO: CREATE MIDDLEWARE TO VALIDATE IF ROLE IS ADMIN OR OPERATOR
+/**
+ * @swagger
+ * /api/incident/{id}:
+ *   get:
+ *     summary: Get report by Id
+ *     tags: [Report]
+ *     parameters:
+ *     - name: id
+ *       in: path
+ *       description: Report ID.
+ *       required: true
+ *       schema:
+ *        type: string
+ *     responses:
+ *       200:
+ *         description:  
+ *         content:
+ *           application/json:
+ *             example:
+ *               incident:
+ *                  id: 1
+ *                  incidentName: Trash
+ *                  description: Swagger
+ *                  incidentEvidence: base64....
+ *               statusCode: 200
+ *       404:
+ *         description: Report not found
+ *       422:
+ *         description: Fields Error
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                errors:
+ *                  type: object
+ *                  example: {"errors":[{"field":"Id","message":{"requiredType":"string","warnings":"The field does not exist, is not a string or is empty."}}]}
+ */
 router.get(
     '/:incidentId',
     getIncidentValidationRules(),
@@ -17,6 +54,56 @@ router.get(
     getIncident
 );
 
+/**
+ * @swagger
+ * /api/incident/create:
+ *   post:
+ *     summary: Create report
+ *     tags: [Report]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              required:
+ *                - incidentName
+ *                - description
+ *              properties:
+ *               incidentName:
+ *                   type: string
+ *                   description: Title about the report.
+ *                   example: Basura tirada en casa 401
+ *               description:
+ *                   type: string
+ *                   description: Details about the report.
+ *                   example: Basura regada en casa 401
+ *               incidentEvidence:
+ *                   type: Integer
+ *                   description: Photo about the incident reported. Is optional.
+ *                   example: base64...
+ *                  
+ *     responses:
+ *       200:
+ *         description:  
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 1
+ *               statusCode: 200
+ *       400:
+ *         description: Unkown error
+ *       422:
+ *         description: Fields Error
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                errors:
+ *                  type: object
+ *                  example: {"errors":[{"field":"description","message":{"requiredType":"string","warnings":"The field does not exist, is not a string or is empty."}}]}
+ */
 router.post(
     '/create',
     createIncidentValidationRules(),
@@ -26,6 +113,55 @@ router.post(
     createIncident
 );
 
+/**
+ * @swagger
+ * /api/incident/update:
+ *   put:
+ *     summary: Update report
+ *     tags: [Report]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              required:
+ *                - incidentName
+ *                - description
+ *              properties:
+ *               incidentName:
+ *                   type: string
+ *                   description: Title about the report.
+ *                   example: Basura tirada en casa 401
+ *               description:
+ *                   type: string
+ *                   description: Details about the report.
+ *                   example: Basura regada en casa 401
+ *               incidentEvidence:
+ *                   type: Integer
+ *                   description: Photo about the incident reported. Is optional.
+ *                   example: base64...
+ *                  
+ *     responses:
+ *       200:
+ *         description:  
+ *         content:
+ *           application/json:
+ *             example:
+ *               statusCode: 200
+ *       400:
+ *         description: Unkwon error
+ *       422:
+ *         description: Fields Error
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                errors:
+ *                  type: object
+ *                  example: {"errors":[{"field":"description","message":{"requiredType":"string","warnings":"The field does not exist, is not a string or is empty."}}]}
+ */
 router.put(
     '/update',
     updateIncidentValidationRules(),
@@ -35,6 +171,39 @@ router.put(
     updateIncident
 );
 
+/**
+ * @swagger
+ * /api/incident/{id}:
+ *   delete:
+ *     summary: Delete report by Id
+ *     tags: [Report]
+ *     parameters:
+ *     - name: id
+ *       in: path
+ *       description: Report ID.
+ *       required: true
+ *       schema:
+ *        type: string
+ *     responses:
+ *       200:
+ *         description:  
+ *         content:
+ *           application/json:
+ *             example:
+ *               statusCode: 200
+ *       400:
+ *         description: Unkwon error
+ *       422:
+ *         description: Fields Error
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                errors:
+ *                  type: object
+ *                  example: {"errors":[{"field":"Id","message":{"requiredType":"string","warnings":"The field does not exist, is not a string or is empty."}}]}
+ */
 router.delete(
     '/delete/:incidentId',
     getIncidentValidationRules(),
@@ -44,6 +213,51 @@ router.delete(
     deleteIncident
 );
 
+/**
+ * @swagger
+ * /api/incident/incidents:
+ *   post:
+ *     summary: Get reports paginated
+ *     tags: [Report]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *               pageSize:
+ *                   type: integer
+ *                   description: Number of documents per page, by default is 10 documents per page
+ *                   example: 15
+ *               page:
+ *                   type: Integer
+ *                   description: Number of page, by default will be the page number 1
+ *                   example: 1
+ *     responses:
+ *       200:
+ *         description:  
+ *         content:
+ *           application/json:
+ *             example:
+ *               data: [{"id":3,"incidentName":"test","description":"test","incidentEvidence":"test 3"},{"id":1,"incidentName":"test","description":"test","incidentEvidence":null}]
+ *               count: 10 
+ *               page: 1
+ *               pages: 3
+ *               statusCode: 200
+ *       400:
+ *         description: Unkwon error
+ *       422:
+ *         description: Fields Error
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                errors:
+ *                  type: object
+ *                  example: {"errors":[{"field":"pageSize","message":{"requiredType":"string","warnings":"The field does not exist, is not a string or is empty."}}]}
+ */
 router.post(
     '/incidents',
     getIncidentsValidationRules(),
