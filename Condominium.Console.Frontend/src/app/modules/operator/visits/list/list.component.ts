9b7d7fb93b5c019b7d9b0cd4f8a78f47c10f1448
Javier Martínez <jmartinez@develsystems.com>
2023-04-27 21:01:35 -0600

    [UPDATE] Visits module UI started

---
+++ Condominium.Console.Frontend/src/app/modules/operator/visits/list/list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'visit-list',
+  templateUrl: './list.component.html',
+  styles: [
+  ]
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
